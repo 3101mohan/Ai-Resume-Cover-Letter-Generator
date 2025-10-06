3b166ed165b822d811e28723a252e2fe259f8980
Mandala Mohansai Subramanyam <98369749+3101mohan@users.noreply.github.com>
2025-10-06 17:57:25 +0530

    Delete mm.py

--- mm.py
+++
@@ -1,8 +0,0 @@
-from google import genai
-
-client = genai.Client(api_key="AIzaSyAdAP0MN9DhRUYHTZLZ5cEu3N9DFBexAss")
-
-response = client.models.generate_content(
-    model="gemini-2.5-flash", contents="Explain how AI works in a few words"
-)
-print(response.text)
